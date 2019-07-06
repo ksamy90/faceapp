c14b7ac48e92ede0fa5cb9b8988d72c7330df446
ksamy90 <ksamy90@aol.com>
2019-07-05 20:40:19 -0700

    Add firebase region

--- functions/index.js
+++ functions/index.js
@@ -30,7 +30,7 @@ app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
@@ -44,4 +44,4 @@ app.post('/scream', (req, res) => {
         });
 });
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
