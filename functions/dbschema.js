82210e632509fa3adab291f6491386b4d2d69f87
ksamy90 <ksamy90@aol.com>
2019-07-05 20:41:15 -0700

    Add database schema

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2019-07-06T03:29:28.243Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+};
